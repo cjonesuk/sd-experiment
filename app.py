96fdfd053d4511751697ec0107fccbb05a3e8923
Chris Jones <chrisjones170187@gmail.com>
2024-03-18 12:27:15 +0000

    inpaint experiment, tabs

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import gradio as gr
 from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionInpaintPipeline
 from diffusers import (
     DDPMScheduler,
     DDIMScheduler,
@@ -20,6 +21,7 @@ print(os.getcwd())
 
 repo_id = "runwayml/stable-diffusion-v1-5" 
 cyberrealistic = "sd15/cyberrealistic_v42.safetensors"
+analogMadnessInpainting = "sd15/analogMadness_v70-inpainting.safetensors"
 model_path = "models/checkpoints"
 model_full_path = model_path + '/' + cyberrealistic
  
@@ -47,9 +49,19 @@ def generate_image(prompt, negative_prompt, num_inference_steps, guidance_scale)
 
     return image
 
-with gr.Blocks() as demo:
-    gr.Markdown("# Experiment with Stable Diffusion V1.5")
 
+def inpaint_image(init_image, mask_image, prompt, negative_prompt, num_inference_steps, guidance_scale):
+    print("Inpainting image...")
+    pipeline = StableDiffusionInpaintPipeline.from_single_file(model_full_path, torch_dtype=torch.float16)
+ 
+    #pipeline.scheduler = EulerDiscreteScheduler.from_config(pipeline.scheduler.config)
+
+    pipeline = pipeline.to("cuda")
+
+    image = pipeline(prompt=prompt, negative_prompt=negative_prompt, image=init_image, mask_image=mask_image).images[0]
+    return image
+
+def define_generate_ui():
     with gr.Row():
         with gr.Column():
             prompt = gr.Textbox("", label="Enter a prompt", placeholder="a photo of an astronaut riding a horse on mars", lines=3)
@@ -65,5 +77,38 @@ with gr.Blocks() as demo:
 
     generate.click(generate_image, inputs=[prompt, negative_prompt, num_inference_steps, guidance_scale], outputs=res)
 
+
+def define_inpaint_ui():
+    with gr.Row():
+        with gr.Column():
+            prompt = gr.Textbox("", label="Enter a prompt", placeholder="a photo of an astronaut riding a horse on mars", lines=3)
+            negative_prompt = gr.Textbox("", label="Enter a negative prompt", placeholder="low quality, lowres", lines=3)
+
+            with gr.Row():
+                init_image= gr.ImageEditor(label="Initial Image")
+                mask_image = gr.ImageEditor(label="Mask Image")
+
+            with gr.Row():
+                num_inference_steps = gr.Number(30, label="Steps", minimum=1, maximum=100, step=1)
+                guidance_scale = gr.Number(8.0, label="Guidance Scale", minimum=0.0, maximum=20.0, step=0.1)
+
+            inpaint = gr.Button("Inpaint Image")
+
+        res = gr.Image(label="output")
+
+    inpaint.click(inpaint_image, inputs=[init_image, mask_image, prompt, negative_prompt, num_inference_steps, guidance_scale], outputs=res)
+
+
+
+with gr.Blocks() as demo:
+    gr.Markdown("# Experiment with Stable Diffusion V1.5")
+
+    with gr.Tabs():
+        with gr.Tab("Generate"):
+            define_generate_ui()
+           
+        with gr.Tab("Inpaint"):
+            define_inpaint_ui()
+           
 if __name__ == "__main__":
     demo.launch()
